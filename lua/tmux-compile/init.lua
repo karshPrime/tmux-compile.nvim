16e5484fb02f224e9fde15f8915ea2ac999d0443
karsh <utkarsh@cyberfear.com>
2024-06-18 00:33:04 +1000

    code optimise & restructure
    ~ restructured most of the initial source code, removing a lot of
      redundant code

--- lua/tmux-compile/init.lua
+++ lua/tmux-compile/init.lua
@@ -14,14 +14,12 @@ end
 
 -- confirm tmux is installed
 local function is_tmux_installed()
-    local output = vim.fn.system("which tmux 2>/dev/null")
-    return output ~= "" and not string.match(output, "tmux not found")
+    return vim.fn.executable("tmux") == 1
 end
 
 -- check if session is in tmux
 local function is_tmux_running()
-  local output = vim.fn.system("tmux info 2>/dev/null")
-  return output ~= "" and not string.match(output, "no client running")
+    return vim.env.TMUX ~= nil
 end
 
 
@@ -30,72 +28,60 @@ end
 -- get the file extension
 local function get_file_extension()
     local filename = vim.api.nvim_buf_get_name(0)
-    if filename == "" then return nil end
     return filename:match("^.+(%..+)$"):sub(2)
 end
 
 -- get build and run commands based on file extension
 local function get_commands_for_extension(extension)
-    local fileExtension = get_file_extension()
-    if fileExtension then
+    if extension then
         for _, cfg in ipairs(M.config) do
             if vim.tbl_contains(cfg.extension, extension) then
                 return cfg.build, cfg.run
             end
         end
-        print("No build and run commands found for this extension")
+        print("Error: No build and run commands found for this extension")
     else
-        print("No file extension found")
+        print("Error: No file extension found")
     end
     return nil, nil
 end
 
 -- check if a tmux window with the given name exists
 local function tmux_window_exists(window_name)
-    local handle = io.popen("tmux list-windows | grep -w " .. window_name)
-    local result = handle:read("*a")
-    handle:close()
+    local result = vim.fn.system("tmux list-windows | grep -w " .. window_name)
     return result ~= ""
 end
 
 
 --# CALL FUNCTIONS #------------------------------------------------------------
 
--- function to run the command in a new or existing tmux window
-function M.new_window(cmd)
-    if cmd then
-        local window_name = "build"
+-- run command in a new or existing tmux window
+local function new_window(cmd, new_cmd)
+    local window_name = "build"
 
-        if tmux_window_exists(window_name) then
-            -- ensure build is in same directory as the project
-            local proj_dir = vim.fn.system("pwd")
-            proj_dir = vim.fn.trim(proj_dir)
+    if tmux_window_exists(window_name) then
+        local proj_dir = vim.fn.trim(vim.fn.system("pwd"))
+        local win_dir = vim.fn.trim(vim.fn.system("tmux display -p -t " .. window_name .. " '#{pane_current_path}'"))
 
-            local win_dir = vim.fn.system("tmux display -p -t 'build'  '#{pane_current_path}'")
-            win_dir = vim.fn.trim(win_dir)
-
-            if (win_dir ~= proj_dir) then 
-                cmd = "cd " .. proj_dir .. "; " .. cmd
-            end
-
-            -- run the command
-            local cmd_head = "silent !tmux select-window -t " .. window_name
-            vim.cmd(cmd_head .. " \\; send-keys '" .. cmd .. "' C-m")
-        else
-            local cmd_head = "silent !tmux new-window -n " .. window_name
-            vim.cmd(cmd_head .. " '" .. cmd .. "; zsh'")
+        if win_dir ~= proj_dir then
+            cmd = "cd " .. proj_dir .. "; " .. cmd
         end
+
+        local cmd_head = "silent !tmux select-window -t " .. window_name
+        vim.cmd(cmd_head .. " \\; send-keys '" .. cmd .. "' C-m")
     else
-        print("No run command found for this extension")
+        local cmd_head = "silent !tmux new-window -n " .. window_name
+        vim.cmd(cmd_head .. " '" .. cmd .. "; zsh'")
     end
 end
 
-function M.same_window(cmd, side)
+-- run command in same window on a new pane
+local function split_window(cmd, side)
     if cmd then
-        local cmd_head = "silent !tmux split-window "
-        vim.cmd(cmd_head .. side .. " -v '" .. cmd .. "; exec zsh'")
+        local cmd_head = "silent !tmux split-window " .. side
+        vim.cmd(cmd_head .. " '" .. cmd .. "; exec zsh'")
     else
-        print("No run command found for this extension")
+        print("Error: No run command found for this extension")
     end
 end
 
@@ -104,35 +90,33 @@ end
 
 -- call the appropriate function based on the option
 function M.dispatch(option)
-
-    -- confirm tmux is installed
     if not is_tmux_installed() then
         print("Error: install TMUX to use the plugin")
         return 1
     end
 
-    -- check if tmux running, else print error
     if not is_tmux_running() then
         print("Error: run session in TMUX")
         return 1
     end
 
-    local make, run = get_commands_for_extension(get_file_extension())
+    local extension = get_file_extension()
+    local make, run = get_commands_for_extension(extension)
 
     if option == "RunBG" then
-        M.new_window(run)
+        new_window(run, true)
     elseif option == "RunV" then
-        M.same_window(run, "-v")
+        split_window(run, "-v")
     elseif option == "RunH" then
-        M.same_window(run, "-h")
+        split_window(run, "-h")
     elseif option == "MakeV" then
-        M.same_window(make, "-v")
+        split_window(make, "-v")
     elseif option == "MakeH" then
-        M.same_window(make, "-h")
+        split_window(make, "-h")
     elseif option == "Make" then
-        M.new_window(make)
+        new_window(make, true)
     else
-        print("Invalid option. Please use one of: RunBG, RunV, RunH, Make")
+        print("Error: Invalid option. Please use one of: RunBG, RunV, RunH, Make")
     end
 end
 
@@ -141,7 +125,7 @@ vim.api.nvim_create_user_command('TMUXcompile', function(args)
     M.dispatch(args.args)
 end, {
     nargs = 1,
-    complete = function(arglead, cmdline, cursorpos)
+    complete = function()
         return { "RunBG", "RunV", "RunH", "Make" }
     end,
 })
