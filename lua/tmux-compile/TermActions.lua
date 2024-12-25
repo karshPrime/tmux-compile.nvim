09bb92f1111b9df6957caa87cf3f925247056fcd
karsh <utkarsh@cyberfear.com>
2024-12-26 09:20:58 +1100

    feat: split term
    + added logic to split term panes to create new vim pane on the same
      window to perform the triggered command.
    * it doesn't yet reuse existing panes!

--- lua/tmux-compile/TermActions.lua
+++ lua/tmux-compile/TermActions.lua
@@ -6,7 +6,6 @@ local Env = require( "tmux-compile.Env" )
 
 local Actions = {}
 
-
 --
 -- run command in a new or existing tmux window
 function Actions.new_window( aCmd, aWindowTitle, aErrorName )
@@ -32,7 +31,6 @@ function Actions.overlay( aCmd, aSleepDuration, aWidth, aHeight, aErrorName )
 
 end
 
-
 --
 -- run command in same window on a new pane
 function Actions.split_window( aCmd, aSide, aWidth, aHeight, aNewPane, aErrorName )
@@ -43,8 +41,34 @@ function Actions.split_window( aCmd, aSide, aWidth, aHeight, aNewPane, aErrorNam
         return 1
     end
 
-end
+    if aSide == "h" then
+        local lCurrentWin = vim.api.nvim_get_current_win()
+        vim.cmd( "wincmd l" )
+        local lMovedWin = vim.api.nvim_get_current_win()
 
+        if lCurrentWin == lMovedWin or aNewPane then
+            lWidth = math.floor( vim.api.nvim_win_get_width(0) * aWidth / 100 )
+            vim.cmd( lWidth .. "vs" )
+        end
+    else
+        local lCurrentWin = vim.api.nvim_get_current_win()
+        vim.cmd( "wincmd j" )
+        local lMovedWin = vim.api.nvim_get_current_win()
+
+        if lCurrentWin == lMovedWin or aNewPane then
+            lHeight = math.floor( vim.api.nvim_win_get_height(0) * aHeight / 100 )
+            vim.cmd( lHeight .. " split" )
+        end
+    end
+
+    vim.cmd( "term " .. aCmd )
+    vim.wo.scrolloff = 0
+    vim.wo.sidescrolloff = 0
+    vim.wo.number = false
+    vim.wo.relativenumber = false
+
+    vim.cmd( "wincmd p" )
+end
 
 return Actions
 
